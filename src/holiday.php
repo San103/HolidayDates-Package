daeb68ab75ea7afc7f21879269b69f32ff2338b5
San103 <palbanjessan16@gmail.com>
2023-12-07 08:46:08 +0800

    adding api

--- src/holiday.php
+++ src/holiday.php
@@ -1,9 +1,189 @@
 <?php
 
 namespace San103\Phpholidayapi;
-
+use GuzzleHttp\Client;
 class holiday{
-    public function try(){
-        return 'this works';
+    protected $api;
+    protected $apiKey;
+    // private static int $gapi = ;
+    public function countryCode($code = 'philippines'){
+       $this->api = "https://www.googleapis.com/calendar/v3/calendars/en.{$code}%23holiday%40group.v.calendar.google.com/events?key=";
+        return $this;
+    }
+
+    public function apiKey($key = 'AIzaSyCxa_wfS4ITyuGgbPh4P4SQjQI942bHGx0'){
+       $this->apiKey = $key;
+       return $this;
+    }
+
+   
+
+    public function result(){
+
+        $client = new Client();
+            try {
+                $response = $client->get($this->api . $this->apiKey);
+
+                if ($response->getStatusCode() == 200) {
+
+                    // $toJson = collect($response->json()['items']);
+                    $jsonData = json_decode($response->getBody(), true);
+
+                    // $arrayFilter = $toJson->filter(function ($item, $key) {
+                    //     return $item['description'] === 'Public holiday' && date('Y', strtotime($item['start']['date'])) === date('Y') && $item['status'] === 'confirmed';
+                    // });
+
+                    $filteredData = array_filter($jsonData['items'], function ($item) {
+                        return $item['description'] === 'Public holiday'
+                            && date('Y', strtotime($item['start']['date'])) === date('Y')
+                            && $item['status'] === 'confirmed';
+                    });
+
+                    $mappedData = array_map(function ($item) {
+                        return [
+                            'id' => $item['id'],
+                            'title' => $item['summary'],
+                            'description' => $item['description'],
+                            'start' => $item['start'],
+                            'end' => $item['end'],
+                        ];
+                    }, $filteredData);
+
+                    return $mappedData;
+                }
+                return self::localHolidays();
+            } catch (Exception) {
+                return self::localHolidays();
+            }
+    }
+
+
+    public static function localHolidays()
+    {
+        return array(
+            [
+                [
+                    "title" => "New Year's Day",
+                    "start" => [
+                        "date" => "2023-01-01"
+                    ]
+                ],
+                [
+                    "title" => "People Power Anniversary",
+                    "start" => [
+                        "date" => "2023-02-25"
+                    ]
+                ],
+                [
+                    "title" => "Maundy Thursday",
+                    "start" => [
+                        "date" => "2023-04-06"
+                    ]
+                ],
+                [
+                    "title" => "Good Friday",
+                    "start" => [
+                        "date" => "2023-04-07"
+                    ]
+                ],
+                [
+                    "title" => "Black Saturday",
+                    "start" => [
+                        "date" => "2023-04-08"
+                    ]
+                ],
+                [
+                    "title" => "Labor Day",
+                    "start" => [
+                        "date" => "2023-05-01"
+                    ]
+                ],
+                [
+                    "title" => "Independence Day",
+                    "start" => [
+                        "date" => "2023-06-12"
+                    ]
+                ],
+                [
+                    "title" => "Ninoy Aquino Day",
+                    "start" => [
+                        "date" => "2023-08-21"
+                    ]
+                ],
+                [
+                    "title" => "National Heroes Day",
+                    "start" => [
+                        "date" => "2023-08-28"
+                    ]
+                ],
+                [
+                    "title" => "Special non-working day after New Year",
+                    "start" => [
+                        "date" => "2023-01-02"
+                    ]
+                ],
+                [
+                    "title" => "The Day of Valor",
+                    "start" => [
+                        "date" => "2023-04-10"
+                    ]
+                ],
+                [
+                    "title" => "Day off for People Power Anniversary",
+                    "start" => [
+                        "date" => "2023-02-24"
+                    ]
+                ],
+                [
+                    "title" => "Eidul-Fitar Holiday",
+                    "start" => [
+                        "date" => "2023-04-21"
+                    ]
+                ],
+                [
+                    "title" => "Eid al-Adha (Feast of the Sacrifice)",
+                    "start" => [
+                        "date" => "2023-06-28"
+                    ]
+                ],
+                [
+                    "title" => "All Saints' Day",
+                    "start" => [
+                        "date" => "2023-11-01"
+                    ]
+                ],
+                [
+                    "title" => "All Souls' Day",
+                    "start" => [
+                        "date" => "2023-11-02"
+                    ]
+                ],
+                [
+                    "title" => "Day off for Bonifacio Day",
+                    "start" => [
+                        "date" => "2023-11-27"
+                    ]
+                ],
+                [
+                    "title" => "Christmas Day",
+                    "start" => [
+                        "date" => "2023-12-25"
+                    ]
+                ],
+                [
+                    "title" => "Rizal Day",
+                    "start" => [
+                        "date" => "2023-12-30"
+                    ]
+                ],
+                [
+                    "title" => "New Year's Eve",
+                    "start" => [
+                        "date" => "2023-12-31"
+                    ]
+                ]
+            ]
+
+        );
     }
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,121 @@
+<?php return array(
+    'root' => array(
+        'name' => 'san103/php-holiday-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fb68a098a7c087e7b82dded59773254c0b28f81d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'guzzlehttp/guzzle' => array(
+            'pretty_version' => '7.9.x-dev',
+            'version' => '7.9.9999999.9999999-dev',
+            'reference' => '41042bc7ab002487b876a0683fc8dce04ddce104',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/promises' => array(
+            'pretty_version' => '2.0.x-dev',
+            'version' => '2.0.9999999.9999999-dev',
+            'reference' => 'bbff78d96034045e58e13dedd6ad91b5d1253223',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/promises',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/psr7' => array(
+            'pretty_version' => '2.6.x-dev',
+            'version' => '2.6.9999999.9999999-dev',
+            'reference' => '45b30f99ac27b5ca93cb4831afe16285f57b8221',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/psr7',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bb5906edc1c324c9a05aa0873d40117941e5fa90',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-client',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-factory' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7037f4b0950474e9d1350e8df89b15f1842085f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-factory',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-factory-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '402d35bcb92c70c026d1a6a9883f06b2ead23d71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(
+                0 => '2.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-message-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'ralouphie/getallheaders' => array(
+            'pretty_version' => '3.0.3',
+            'version' => '3.0.3.0',
+            'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ralouphie/getallheaders',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'san103/php-holiday-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fb68a098a7c087e7b82dded59773254c0b28f81d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/deprecation-contracts' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c3aff79d10325257a001fcf92d991f24fc967cf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/deprecation-contracts',
+            'aliases' => array(
+                0 => '3.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
